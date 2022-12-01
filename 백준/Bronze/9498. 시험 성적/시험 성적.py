9da3c7d67cb029b52a001ca6e5e512c1e2c500d5
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 19:19:08 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+input_score = int(input())
+
+if 90 <= input_score <= 100:
+    print("A")
+elif 80 <= input_score <= 89:
+    print("B")
+elif 70 <= input_score <= 79:
+    print("C")
+elif 60 <= input_score <= 69:
+    print("D")
+else:
+    print("F")
