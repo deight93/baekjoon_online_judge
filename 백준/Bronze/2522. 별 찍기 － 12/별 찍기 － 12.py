74aa4d135ff569a5ba4ad82dae185d5e60dfdcc8
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 18:16:27 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 72 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+temp2 = []
+for i in range(1, n+1):
+    temp = " "*(n-i)
+    temp += "*"*i
+    temp2 += [temp]
+
+for j in temp2[:-1]:
+    print(j)
+
+for j in temp2[::-1]:
+    print(j)
