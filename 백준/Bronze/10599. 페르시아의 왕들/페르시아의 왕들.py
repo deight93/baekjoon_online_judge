7909d4b999a184fc4a089badf6b76270d5039c43
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 19:29:55 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 64 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.py
@@ -0,0 +1,11 @@
+
+import sys
+
+while True:
+    a, b, c, d = map(int, sys.stdin.readline().rstrip().split(" "))
+    if a == 0 and b == 0 and c == 0 and d == 0:
+        break
+    else:
+        min_age = c - b
+        max_age = d - a
+        print(min_age, max_age)
