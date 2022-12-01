3c0649ecc919c2dab0edd5a90568dcf178eb1742
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 20:01:27 +0900

    [Silver IV] Title: 숫자세는 양 (Small), Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/14381. 숫자세는 양 （Small）/숫자세는 양 （Small）.py
@@ -0,0 +1,20 @@
+import sys
+
+t = int(sys.stdin.readline().rstrip())
+temp = [i for i in "0123456789"]
+
+for i in range(t):
+    n = int(sys.stdin.readline().rstrip())
+    if n == 0:
+        print("Case #{}: INSOMNIA".format(i+1))
+    else:
+        c = 0
+        c_temp = []
+        while True:
+            c += 1
+            for j in str(c*n):
+                c_temp.append(j)
+
+            if temp == sorted(list(set(c_temp))):
+                break
+        print("Case #{}: {}".format(i + 1, c*n))
