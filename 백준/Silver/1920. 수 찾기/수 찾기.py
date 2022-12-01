88f107be8367b4c7c3b598bc890bac26ed4d3aee
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 17:00:11 +0900

    [Silver IV] Title: 수 찾기, Time: 272 ms, Memory: 153600 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+a = sys.stdin.readline().rstrip().split(" ")
+m = int(sys.stdin.readline())
+temp = sys.stdin.readline().rstrip().split(" ")
+temp2 = set(temp)-set(a)
+for i in temp:
+    if i in temp2:
+        print(0)
+    else:
+        print(1)
