268241ce4ed519641ddce1b2c472f469343ff685
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 19:41:54 +0900

    [Bronze V] Title: A+B - 8, Time: 84 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,6 @@
+import sys
+
+c = int(input())
+for i in range(1, c+1):
+    input_list = [int(i) for i in sys.stdin.readline().rstrip().split(" ")]
+    print("Case #{}: {} + {} = {}".format(i, input_list[0], input_list[1], input_list[0]+input_list[1]))
