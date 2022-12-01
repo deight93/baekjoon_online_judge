f09aa303cd555464177dd01ffd7183540182506e
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 19:36:00 +0900

    [Silver IV] Title: 큐, Time: 96 ms, Memory: 34024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,33 @@
+import collections
+import sys
+
+dq = collections.deque()
+n = int(sys.stdin.readline())
+cmd_list = [sys.stdin.readline().rstrip().split(" ") for _ in range(n)]
+
+for i in cmd_list:
+    if i[0] == "push":
+        dq.append(int(i[1]))
+    elif i[0] == "front":
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[0])
+    elif i[0] == "pop":
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[0])
+            dq.popleft()
+    elif i[0] == "size":
+        print(len(dq))
+    elif i[0] == "empty":
+        if len(dq) == 0:
+            print(1)
+        else:
+            print(0)
+    elif i[0] == "back":
+        if len(dq) == 0:
+            print(-1)
+        else:
+            print(dq[-1])
