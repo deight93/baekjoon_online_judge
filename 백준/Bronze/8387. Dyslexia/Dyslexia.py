51fe4d1493bfda2b72cc9e4de2166b43a3c74dee
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 19:12:13 +0900

    [Bronze III] Title: Dyslexia, Time: 84 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/8387. Dyslexia/Dyslexia.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+a = sys.stdin.readline().rstrip()
+b = sys.stdin.readline().rstrip()
+
+cnt = 0
+for i in range(n):
+    if a[i] == b[i]:
+        cnt += 1
+print(cnt)
