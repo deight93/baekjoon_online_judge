c8f03b86a4535817a49f5a6530d469cf1e387df9
D8 <37678115+deight93@users.noreply.github.com>
2022-12-01 18:03:14 +0900

    [Bronze IV] Title: 주사위 세개, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,16 @@
+
+import sys
+
+input_list = [int(i) for i in sys.stdin.readline().split()]
+
+if len(set(input_list)) == 1:
+    temp = 10000+input_list[0]*1000
+elif len(set(input_list)) == 2:
+    set_list = list(set(input_list))
+    input_list.remove(set_list[0])
+    input_list.remove(set_list[1])
+    temp = 1000+input_list[0]*100
+else:
+    temp = max(input_list)*100
+
+print(temp)
