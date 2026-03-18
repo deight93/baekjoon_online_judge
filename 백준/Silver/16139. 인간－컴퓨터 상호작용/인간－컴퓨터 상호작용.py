19cc609e32813237e79a099fe67ee430a300ff5b
Donghwan-Kim <37678115+deight93@users.noreply.github.com>
2026-03-18 12:00:16 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 152 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -5,9 +5,11 @@ prefix = [[0] * (len(s) + 1) for _ in range(26)]
 
 for i in range(len(s)):
     idx = ord(s[i]) - ord('a')
+    prefix[idx][i+1] = prefix[idx][i] + 1
+
     for j in range(26):
-        prefix[j][i+1] = prefix[j][i]
-    prefix[idx][i+1] += 1
+        if j != idx:
+            prefix[j][i+1] = prefix[j][i]
 
 for _ in range(n):
     a, l, r = input().split()
