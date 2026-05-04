33c8a80c309a0ddfb165a0434d62dcd672062ce3
Donghwan-Kim <37678115+deight93@users.noreply.github.com>
2026-05-04 10:44:11 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,10 @@
+def solution(a, b, c):
+    answer = 0
+    if len({a, b, c}) == 3:
+        answer = a + b + c
+    elif len({a, b, c}) == 1:
+        answer = (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3 )
+    else:
+        answer = (a + b + c) * (a**2 + b**2 + c**2 )
+    
+    return answer
