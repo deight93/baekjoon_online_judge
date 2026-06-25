a8be9f287053a8b24e7fea399a2935a7dbdf2c90
Donghwan-Kim <37678115+deight93@users.noreply.github.com>
2026-06-25 13:23:36 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,11 @@
+def solution(str_list):
+    answer = []
+    
+    for i, s in enumerate(str_list):
+        if s == "l":
+            answer = str_list[:i]
+            break
+        elif s == "r":
+            answer = str_list[i+1:]
+            break
+    return answer
